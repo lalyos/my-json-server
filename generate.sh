d2c996862cc81cbc99a12c91b35b6b0056d415cf
lalyos <lalyos@yahoo.com>
2017-11-06 07:56:50 +0100

    generate only boulders to fit 10kb

--- generate.sh
+++ generate.sh
@@ -1,1 +1,1 @@
-jq '{boulders: [.[].boulders[]|delpaths([["flash"]])]|unique_by(.name),ranks: . }'  ~/prj-old/prj/ujjero/src/main/webapp/js/hiscore-2016-02-11.json > db.json
+jq '{boulders: [.[].boulders[]|delpaths([["flash"]])]|unique_by(.name)}'  ~/prj-old/prj/ujjero/src/main/webapp/js/hiscore-2016-02-11.json > db.json
